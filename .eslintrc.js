c45b8b4cb7d838b03de85a9fadfb35895d097770
dsrkafuu <dsrkafuu@outlook.com>
2021-02-27 18:17:14 +0800

    chore: update linter configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,6 @@ module.exports = {
   },
   rules: {
     'no-console': 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
   },
 };
